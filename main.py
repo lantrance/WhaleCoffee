f901e702e8f52bb8c033c049fb803420667c78c0
lantrance <133297522+lantrance@users.noreply.github.com>
2025-02-23 11:05:37 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,16 @@
+from website import create_app
+
+
+app = create_app()
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+
+
+
+# if __name__ == '__main__':
+#    app.run(debug=True, host='172.20.10.3')
+
